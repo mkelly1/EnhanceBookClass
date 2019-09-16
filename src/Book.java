f379cbec1e82c3f8fddad35b5810dfe569ad2cab
mkelly1 <mkelly@his.com>
2019-09-16 17:13:01 -0400

    EnhanceBookClass

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+
+
 public class Book {
     private String sku;
     private String title;
@@ -5,8 +7,19 @@ public class Book {
     private String description;
     private double price;
     private boolean isInStock;
+    //private BookDatabase mybooks= new BookDatabase();
 
     public Book(){}
+
+    public Book(String mySku, BookDatabase mybooks){
+        //this.mybooks = mybooks;
+        this.sku = mySku;
+        this.title = mybooks.getBook(sku).getTitle();
+        this.author = mybooks.getBook(sku).getAuthor();
+        this.description = mybooks.getBook(sku).getDescription();
+        this.price = mybooks.getBook(sku).getPrice();
+        this.isInStock = mybooks.getBook(sku).isInStock();
+    }
     public Book(String sku, String title, String author, String descrip, double price, boolean inStock ){
         this.sku = sku;
         this.title = title;
--- src/BookApp.java
+++ src/BookApp.java
@@ -1,10 +1,13 @@
 
 public class BookApp {
     public static void main(String[] args) {
+        String sku;
         BookDatabase bookdB = new BookDatabase();
         double price = bookdB.getInStock() * 20.0;
-        System.out.println("There are "+bookdB.getInStock()+" books in stock.");
-        System.out.println("The total price at $20.00 per book is $"+price);
-        System.out.println(bookdB.getBook("Java1002").getDisplayText());
+        //System.out.println("There are "+bookdB.getInStock()+" books in stock.");
+        //System.out.println("The total price at $20.00 per book is $"+price);
+        Book book = new Book("Orcl1003", bookdB);
+        //User this method to look through db?
+        System.out.println("New book is "+ book.getDisplayText());
     }
 }
